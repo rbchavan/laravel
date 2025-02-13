8624bca98da5353cd4a7690c8e5132b7239f467b
Rahul Chavan <rbchavan@git.edu>
2025-02-13 13:34:35 +0530

    initial commit

---
+++ app/app/Http/Controllers/MyController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\RouteAttributes\Attributes\Get;
+
+class MyController extends Controller
+{
+    #[Get('/profile')]
+    public function profile()
+    {
+        return view('profile');
+    }
+}git@github.com:rbchavan/laravel.git
---
+++ app/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Spatie\RouteAttributes\RouteRegistrar;
+
+Route::middleware('web')->group(function () {
+    (new RouteRegistrar(app()->router))
+        ->useRootNamespace('App\\Http\\Controllers') ;
+        
+});
